91e789d426659f79098b74ee839c119c3a2fdfee
Charleston Wang <wangcharles10@gmail.com>
2024-02-16 17:03:55 -0600

    added short positions, changed structure of access control, added a admin key for administrator control of classes and account objects

--- account.py
+++ account.py
@@ -1,36 +1,105 @@
 import random
 import string
-import typing
+from typing import List
 
 PASSWORD_LENGTH = 16
+ADMIN_KEY = "ADMIN"
 
 class Account:
     """
-    Account class is for each trader, storing their mpid, balance, past_trades, open_positions, and password
+    Account class is for each trader, storing their mpid, balance, past_trades, long_positions, and password
     """
     
-    def __init__(self, mpid, balance=1000000, past_trades=None, open_positions=None, password=None):
-        self._mpid = mpid
-        self._balance = balance
-        self._past_trades = past_trades if past_trades is not None else []
-        self._open_positions = open_positions if open_positions is not None else []
-        self._password = password if password is not None else self._generate_password()
+    def __init__(self, mpid: str, balance: int = 1000000, past_trades: List = None, long_positions: List = None, short_positions: List = None, password: str = None):
+        self.__mpid = mpid
+        self.__balance = balance
+        self.__past_trades = past_trades if past_trades is not None else []
+        self.__long_positions = long_positions if long_positions is not None else []
+        self.__short_positions = short_positions if short_positions is not None else []
+        self.__password = password if password is not None else generate_password()
     
-    def get_balance(self, password):
-        if self._password == password:
-            return self._balance
+    def get_mpid(self, password: str) -> str:
+        """
+        Gets mpid if user password or admin key is provided
+        """
+        if self.__password == password or password == ADMIN_KEY:
+            return self.__mpid
+        else:
+            raise ValueError("Incorrect password")
+        
+    def get_balance(self, password: str) -> int:
+        """
+        Gets the balance if user password or admin key is provided
+        """
+        if self.__password == password or password == ADMIN_KEY:
+            return self.__balance
+        else:
+            raise ValueError("Incorrect password")
+    def set_balance(self, admin_key: str, new_bal: int):
+        """
+        Sets the balance only if admin key is provided
+        """
+        if admin_key == ADMIN_KEY:
+            self.__balance = new_bal
+        else:
+            raise ValueError("Incorrect password")
+        
+    def add_position(self, admin_key: str, trade, long: bool):
+        """
+        Adds position only if admin password is provided
+        Args:
+            admin_key(str): admin password for access
+            trade: underlying trade to add
+            long(bool): TRUE if long position FALSE if short position
+        """
+        if admin_key == ADMIN_KEY:
+            if long:
+                self.__long_positions.append(trade)
+            else:
+                self.__short_positions.append(trade)
+        else:
+            raise ValueError("Incorrect password")
+            
+    def get_long_positions(self, password: str) -> List:
+        """
+        Returns long positions given user password or admin password
+        """
+        if self.__password == password or password == ADMIN_KEY:
+            return self.__long_positions
+        else:
+            raise ValueError("Incorrect password")
+    
+    def get_short_positions(self, password: str) -> List:
+        """
+        Returns short positions given user password or admin password
+        """
+        if self.__password == password or password == ADMIN_KEY:
+            return self.__short_positions
         else:
             raise ValueError("Incorrect password")
 
-    def get_positions(self, password):
-        if self._password == password:
-            return self._open_positions
-
-    def get_past_transactions(self, password):
-        if self._password == password:
-            return self._past_trades
+    def get_past_trades(self, password: str):
+        """
+        Returns pass trades given user password or admin password
+        """
+        if self.__password == password or password == ADMIN_KEY:
+            return self.__past_trades
+        else:
+            raise ValueError("Incorrect password")
     
-    def _generate_password(self):
-        chars = string.ascii_letters + string.digits + string.punctuation
-        password = ''.join(random.choice(chars) for _ in range(PASSWORD_LENGTH))
-        return password
+    def get_password(self, admin_key: str):
+        """
+        Returns user password
+        """
+        if admin_key == ADMIN_KEY:
+            return self.__password
+        else:
+            raise ValueError("Incorrect password")
+    
+def generate_password():
+    """
+    Generates random 16 byte password for account
+    """
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(chars) for _ in range(PASSWORD_LENGTH))
+    return password
--- manager.py
+++ manager.py
@@ -1,7 +1,9 @@
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 import os
 import json
 from account import Account
+
+ADMIN_KEY = "ADMIN"
 
 class Manager:
     """
@@ -15,70 +17,84 @@ class Manager:
         Args:
             filepath (str): local filepath to preexisting account data
         """
-        self._filepath = filepath
-        self._accounts_dict = self.load_accounts()
+        self.__filepath = filepath
+        self.__accounts_dict = self.load_accounts()
 
     def get_mpids(self) -> List[str]:
         """
         Getter for MPIDs
         """
-        return list(self._accounts_dict.keys())
+        return list(self.__accounts_dict.keys())
     
     def get_accounts(self) -> List[Account]:
         """
         Getter for Account objects
         """
-        return list(self._accounts_dict.values())
+        return list(self.__accounts_dict.values())
     
-    def modify_balance(self, mpid, amount):
+    def get_account_dict(self) -> Dict[str, Account]:
+        """
+        Getter for Account dict
+        """
+        return self.__accounts_dict
+    
+    def modify_balance(self, mpid: str, amount: int) -> int:
         """
         Clearing house has access to modify balance of accounts.
         Args:
             mpid: mpid of account to modify balance
             amount: net change of amount to reflect to account
         """
-        if mpid in self._accounts_dict:
-            self._accounts_dict[mpid]._balance += amount
-            return self._accounts_dict[mpid]._balance
+        if mpid in self.__accounts_dict:
+            old_bal = self.__accounts_dict[mpid].get_balance(ADMIN_KEY)
+            self.__accounts_dict[mpid].set_balance(ADMIN_KEY, old_bal + amount)
+            return self.__accounts_dict[mpid].get_balance(ADMIN_KEY)
         else:
             raise ValueError("MPID does not exist")
     
-    def trade_position(self, mpid_b, mpid_s, trade):
-        """
-        Clearing house handles trades and can add positions to Accounts
-        Args:
-            mpid_b: mpid of buyer account to add position
-            mpid_s: mpid of sell account to sell position
-            trade: underlying position to trade
-        """
-        if mpid_b in self._accounts_dict and mpid_s in self._accounts_dict:
-            seller = self._accounts_dict[mpid_s]
-            buyer = self._accounts_dict[mpid_b]
+    # def trade_position(self, mpid_b, mpid_s, trade):
+    #     """
+    #     Clearing house handles trades and can add positions to Accounts
+    #     Args:
+    #         mpid_b: mpid of buyer account to add position
+    #         mpid_s: mpid of sell account to sell position
+    #         trade: underlying position to trade
+    #     """
+    #     if mpid_b in self._accounts_dict and mpid_s in self._accounts_dict:
+    #         seller = self._accounts_dict[mpid_s]
+    #         buyer = self._accounts_dict[mpid_b]
 
-            if trade in seller._open_positions:
-                seller._open_positions.remove(trade)
-                buyer._open_positions.append(trade)
-            else:
-                raise ValueError("Seller does not have position to sell") 
-        else:
-            raise ValueError("Buyer or Seller MPID does not exist")
+    #         if trade in seller._open_positions:
+    #             seller._open_positions.remove(trade)
+    #             buyer._open_positions.append(trade)
+    #         else:
+    #             raise ValueError("Seller does not have position to sell") 
+    #     else:
+    #         raise ValueError("Buyer or Seller MPID does not exist")
     
-    #TODO
-    def settle_positions(self, turn):
+    def add_position(self, mpid: str, trade, long: bool):
         """
-        Clearing house must settle all positions after each turn and update balances, past_trades, and open_positions accordingly
+        Add trade position either long or short to mpid's account
+        Args:
+            mpid: MPID of user to add trade to
+            trade: underlying trade to add
+            long: TRUE if long position FALSE if short position
         """
-        pass
+        if mpid in self.__accounts_dict:
+            self.__accounts_dict[mpid].add_position(ADMIN_KEY, trade, long)
+        else:
+            raise ValueError("MPID does not exist")
+    
 
     def load_accounts(self) -> Tuple[List[Account], List[str]]:
         """
         Loads accounts from preexisting account data from self.filepath
         """
-        if not os.path.exists(self._filepath):
+        if not os.path.exists(self.__filepath):
             return {}
         
         try:
-            with open(self._filepath, 'r', encoding='utf-8') as file:
+            with open(self.__filepath, 'r', encoding='utf-8') as file:
                 data = json.load(file)
         except json.decoder.JSONDecodeError:
             return {}
@@ -86,40 +102,37 @@ class Manager:
         accounts_dict = {mpid: Account(**details) for mpid, details in data.items()}
         return accounts_dict
 
-    def create_account(self):
+    def create_account(self) -> str:
         """
         Create a new Account object with unique mpid in the format "MPID0"
+
+        Returns:
+            password associated with created account
         """
         mpid = "MPID0"  # Starting mpid
-        if self._accounts_dict:
-            last_num = max(int(mpid[4:]) for mpid in self._accounts_dict.keys())
+        if self.__accounts_dict:
+            last_num = max(int(mpid[4:]) for mpid in self.__accounts_dict.keys())
             mpid = f"MPID{last_num + 1}"
 
         new_acc = Account(mpid)
-        self._accounts_dict[mpid] = new_acc
-        return new_acc._password
+        self.__accounts_dict[mpid] = new_acc
+        return new_acc.get_password(ADMIN_KEY)
 
     def save_accounts(self):
         """
         Save account info to file
         """
-        with open(self._filepath, 'w', encoding='utf-8') as file:
+        with open(self.__filepath, 'w', encoding='utf-8') as file:
             data = {mpid: {
-                    "mpid": acc._mpid,
-                    "balance": acc._balance,
-                    "past_trades": acc._past_trades,
-                    "open_positions": acc._open_positions,
-                    "password": acc._password
-                } for mpid, acc in self._accounts_dict.items()}
+                    "mpid": acc.get_mpid(ADMIN_KEY),
+                    "balance": acc.get_balance(ADMIN_KEY),
+                    "past_trades": acc.get_past_trades(ADMIN_KEY),
+                    "long_positions": acc.get_long_positions(ADMIN_KEY),
+                    "short_positions": acc.get_short_positions(ADMIN_KEY),
+                    "password": acc.get_password(ADMIN_KEY)
+                } for mpid, acc in self.__accounts_dict.items()}
             
             json.dump(data, file, indent=4)
-
-    def get_trader(self, mpid):
-        """
-        Retrieve a trader by their mpid
-        """
-        idx = int(mpid[4:])
-        return self._accounts[idx]
 
 # manual testing
 if __name__ == "__main__":
